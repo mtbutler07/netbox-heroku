60abffcbb17ba1ac8cebd4bfa53cfa9e3a95a569
Marcus Butler <mtbutler07@gmail.com>
2019-08-17 18:16:23 -0500

    updated gunicorn config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -c gunicorn_config.py netbox.wsgi
+web: gunicorn -c 'gunicorn_config.py' netbox-heroku.wsgi
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,5 @@
 command = '/usr/bin/gunicorn'
 pythonpath = './netbox/'
-bind = '0.0.0.0:8001'
 workers = 3
 errorlog = '-'
 accesslog = '-'
